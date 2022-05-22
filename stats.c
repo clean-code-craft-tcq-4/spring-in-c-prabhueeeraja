ff8d9b81adfea7a7835d01e587c58b5b4f5d14c2
Prabhakaran Rqaja <prabhueeeraja@gmail.com>
2022-05-22 22:45:52 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -10,12 +10,44 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
 
-float LargestElement(float* arr, int n)
+struct MaxMin
 {
-    for (int i = 1; i < n; ++i) {
-    if (arr[0] < arr[i]) {
-      arr[0] = arr[i];
-    }
+  int min;
+  int max;
+}; 
+ 
+struct MaxMin getMinMax(int arr[], int n)
+{
+  struct MaxMin minmax;    
+  int i;
+  
+  if (n == 1)
+  {
+     minmax.max = arr[0];
+     minmax.min = arr[0];    
+     return minmax;
+  }   
+ 
+  if (arr[0] > arr[1]) 
+  {
+      minmax.max = arr[0];
+      minmax.min = arr[1];
+  } 
+  else
+  {
+      minmax.max = arr[1];
+      minmax.min = arr[0];
+  }   
+ 
+  for (i = 2; i<n; i++)
+  {
+    if (arr[i] >  minmax.max)     
+      minmax.max = arr[i];
+   
+    else if (arr[i] <  minmax.min)     
+      minmax.min = arr[i];
   }
-    return arr[0];
+   
+  return minmax;
 }
+
