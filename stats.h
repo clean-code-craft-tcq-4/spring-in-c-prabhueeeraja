bdb4d2257ba004b0dd9047fcc13c52ce2ff5e790
Prabhakaran Rqaja <prabhueeeraja@gmail.com>
2022-05-30 17:21:08 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -5,11 +5,12 @@ using namespace std;
 
 struct Stats compute_statistics(const float* numberset, int setlength);
 
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
+
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
-extern int emailAlertCallCount;
-extern int ledAlertCallCount;
 
 struct Stats
    	 {
