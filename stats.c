32bd72e9461dbdaeedf567323046889a6047a5bd
Prabhakaran Rqaja <prabhueeeraja@gmail.com>
2022-05-30 17:16:20 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,5 +1,8 @@
 #include "stats.h"
 
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats minmax;
     minmax.average = 0;
@@ -8,38 +11,38 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
   int i;
   float sum = 0;
   
-  if (n == 1)
+  if (setlength == 1)
   {
-     minmax.max = arr[0];
-     minmax.min = arr[0];
-     minmax.max = arr[0];
+     minmax.max = numberset[0];
+     minmax.min = numberset[0];
+     minmax.max = numberset[0];
      return minmax;
   }   
  
-  if (arr[0] > arr[1]) 
+  if (numberset[0] > numberset[1]) 
   {
-      minmax.max = arr[0];
-      minmax.min = arr[1];
+      minmax.max = numberset[0];
+      minmax.min = numberset[1];
   } 
   else
   {
-      minmax.max = arr[1];
-      minmax.min = arr[0];
+      minmax.max = numberset[1];
+      minmax.min = numberset[0];
   }   
  
-  for (i = 2; i<n; i++)
+  for (i = 2; i<setlength; i++)
   {
     if (arr[i] >  minmax.max)     
-      minmax.max = arr[i];
+      minmax.max = numberset[i];
    
     else if (arr[i] <  minmax.min)     
-      minmax.min = arr[i];
+      minmax.min = numberset[i];
   }
   
-  for (i=0; i<n;i++)
+  for (i=0; i<setlength;i++)
    {
       sum=sum+arr[i];
-      minmax.avg=(float)sum/(i+1);
+      minmax.average=(float)sum/(i+1);
    }
   
   return minmax;
